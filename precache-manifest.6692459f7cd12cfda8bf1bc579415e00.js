7e05b2a381ef822704bc2ace4011dbb28a57e1cd
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2019-05-01 08:34:34 +0800

    Updates

---
+++ precache-manifest.6692459f7cd12cfda8bf1bc579415e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319450bdb5072592b7f9d51fb102d186",
+    "url": "/dossiers/index.html"
+  },
+  {
+    "revision": "9b6f4da9a5d226418eb9",
+    "url": "/dossiers/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "d8e5cd4fe9ab165087a9",
+    "url": "/dossiers/static/css/main.4c089c31.chunk.css"
+  },
+  {
+    "revision": "9b6f4da9a5d226418eb9",
+    "url": "/dossiers/static/js/2.7630e0db.chunk.js"
+  },
+  {
+    "revision": "d8e5cd4fe9ab165087a9",
+    "url": "/dossiers/static/js/main.fcf2ac01.chunk.js"
+  },
+  {
+    "revision": "6348e11cb0df66f1995b",
+    "url": "/dossiers/static/js/runtime~main.06b162d8.js"
+  }
+]);
